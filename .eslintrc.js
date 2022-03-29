d893b0cab00dde80eaf9ab13804a40d83766590e
Vishal Devani <vishal.devani@theonetechnologies.co.in>
2022-03-29 16:11:50 +0530

    eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'linebreak-style': 0,
+    'prefer-const': 'error',
+    'prefer-spread': 'error',
+    'func-call-spacing': ['error', 'never'],
+    'no-loop-func': 'error',
+    'no-undef': 2,
+    'react/prop-types': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-extra-non-null-assertion': ['error'],
+    'no-console': ['warn', {}],
+    'JSXeslintreact/react-in-jsx-scope': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-debugger': 'warn',
+    'import/named': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
